20beec6e810bfd6d4ec300edbf82d092bfef280d
Дмитрий Куулар <kuulardmo@bk.ru>
2022-10-05 18:00:35 +0700

    Added right test

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -19,7 +19,7 @@ public class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("/* --> ваша проверка здесь <-- */"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
